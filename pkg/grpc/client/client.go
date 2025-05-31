7115a052906a1e3912c83a6e87e9797f36896a80
LandGAA <BIST10000>
2025-06-01 00:05:47 +0300

    hhh

---
+++ pkg/grpc/client/client.go
@@ -0,0 +1,37 @@
+package client
+
+import (
+	"context"
+	"fmt"
+	pd "github.com/LandGAA/authh2/pkg/grpc/generate"
+	"github.com/LandGAA/authh2/pkg/logger"
+	"go.uber.org/zap"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+	"time"
+)
+
+type Client struct {
+	client pd.UserServiceClient
+}
+
+func NewClient() (*Client, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	conn, err := grpc.DialContext(
+		ctx,
+		"localhost:50051",
+		grpc.WithTransportCredentials(insecure.NewCredentials()),
+		grpc.WithBlock(),
+	)
+	if err != nil {
+		logger.Logger.Error("Ошибка подключения клиента к gRPC серверу",
+			zap.Error(err))
+		return nil, fmt.Errorf("ошибка подключения к gRPC серверу: %v", err)
+	}
+
+	return &Client{
+		client: pd.NewUserServiceClient(conn),
+	}, nil
+}
