706b3e8449384e33cbfce404983da7be44aba92f
LandGAA <BIST10000>
2025-05-31 16:37:08 +0300

    fix fix fix

--- pkg/grpc/client/request.go
+++ pkg/grpc/client/request.go
@@ -7,52 +7,72 @@ import (
 	"github.com/LandGAA/authh2/pkg/logger"
 	"go.uber.org/zap"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
 	"google.golang.org/grpc/metadata"
+	"time"
 )
 
 type Client struct {
-	pd.UserServiceClient
+	conn   *grpc.ClientConn
+	client pd.UserServiceClient
 }
 
 func NewClient() (*Client, error) {
-	conn, err := grpc.Dial("localhost:50051", grpc.WithInsecure())
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	conn, err := grpc.DialContext(
+		ctx,
+		"localhost:50051",
+		grpc.WithTransportCredentials(insecure.NewCredentials()),
+		grpc.WithBlock(),
+	)
 	if err != nil {
-		logger.Logger.Fatal("Ошибка подключения клиента к gRPC серверу",
+		logger.Logger.Error("Ошибка подключения клиента к gRPC серверу",
 			zap.Error(err))
-		return nil, fmt.Errorf("Ошибка подключения клиента к gRPC серверу %v", err)
+		return nil, fmt.Errorf("ошибка подключения к gRPC серверу: %v", err)
 	}
-	defer conn.Close()
-	return &Client{pd.NewUserServiceClient(conn)}, nil
+
+	return &Client{
+		conn:   conn,
+		client: pd.NewUserServiceClient(conn),
+	}, nil
+}
+
+func (c *Client) Close() error {
+	if c.conn != nil {
+		return c.conn.Close()
+	}
+	return nil
 }
 
 func (c *Client) CheckToken(token string) (*pd.UserResponse, error) {
-	ctx := metadata.AppendToOutgoingContext(
-		context.Background(),
-		"authorization", token,
-	)
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
 
-	res, err := c.UserServiceClient.CheckToken(ctx, &pd.TokenRequest{Access: token})
+	ctx = metadata.AppendToOutgoingContext(ctx, "authorization", token)
+
+	res, err := c.client.CheckToken(ctx, &pd.TokenRequest{Access: token})
 	if err != nil {
 		logger.Logger.Error("Ошибка при вызове CheckToken",
 			zap.Error(err))
-		return &pd.UserResponse{}, fmt.Errorf("Ошибка при вызове CheckToken: %v", err)
+		return nil, fmt.Errorf("ошибка проверки токена: %v", err)
 	}
 
 	return res, nil
 }
 
 func (c *Client) GetUserByID(id int) (*pd.UserResponse, error) {
-	user, err := c.UserServiceClient.GetUserByID(context.TODO(), &pd.IDRequest{Id: int64(id)})
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	user, err := c.client.GetUserByID(ctx, &pd.IDRequest{Id: int64(id)})
 	if err != nil {
 		logger.Logger.Error("Ошибка при вызове GetUserByID",
+			zap.Int("id", id),
 			zap.Error(err))
-		return &pd.UserResponse{}, fmt.Errorf("Ошибка при вызове GetUserByID: %v", err)
+		return nil, fmt.Errorf("ошибка получения пользователя: %v", err)
 	}
 
-	return &pd.UserResponse{
-		Id:    user.Id,
-		Role:  user.Role,
-		Email: user.Email,
-		Name:  user.Name,
-	}, nil
+	return user, nil
 }
